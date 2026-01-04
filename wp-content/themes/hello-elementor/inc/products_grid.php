169bbb5c56dcf8191c91599b241bf8d053440fc3
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2026-01-04 23:46:20 +0200

    Update products_grid.php

--- wp-content/themes/hello-elementor/inc/products_grid.php
+++ wp-content/themes/hello-elementor/inc/products_grid.php
@@ -106,7 +106,14 @@ function products_grid_shortcode($atts) {
                     $product_title = get_the_title();
                     $product_link = get_permalink($product_id);
                     $product_image = get_the_post_thumbnail($product_id, 'medium');
-                    $product_price = $product_obj->get_price();
+                    // Get product price in current language/currency
+                    if ( function_exists( 'icl_object_id' ) && function_exists( 'wcml_multi_currency' ) ) {
+                        $product_price = wcml_multi_currency()->prices->get_product_price_in_currency( $product_obj->get_id(), null, true );
+                    } elseif ( function_exists( 'pll_current_language' ) && function_exists( 'wcml_multi_currency' ) ) {
+                        $product_price = wcml_multi_currency()->prices->get_product_price_in_currency( $product_obj->get_id(), null, true );
+                    } else {
+                        $product_price = $product_obj->get_price();
+                    }
                     $regular_price = $product_obj->get_regular_price();
                     $sale_price = $product_obj->get_sale_price();
                     $product_rating = $product_obj->get_average_rating();
