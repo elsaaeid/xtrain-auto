b2996a6bb6b88dceb2732c289753b66ecbb36c35
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2026-01-04 23:44:29 +0200

    Delete wp-content/old-plugins directory

--- wp-content/old-plugins/myfatoorah-woocommerce/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-i18n'), 'version' => '40bc1c6b6bb0a2451f74');
--- wp-content/old-plugins/myfatoorah-woocommerce/includes/templates/paymentFieldsEmbedded.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($this->gateways['form']) {
-    return include_once('sectionForm.php');
-}
-?>
-<script>
-    jQuery('.payment_method_myfatoorah_embedded').remove();
-</script>
